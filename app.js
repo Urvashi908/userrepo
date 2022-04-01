d1de58a510df282ade7cb1b79df4b40d2aadb4c7
Urvashi908 <95474230+Urvashi908@users.noreply.github.com>
2022-04-01 12:50:53 +0530

    hchfwet\

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express")
-const bodyparser= require("body-parser")
-const userrout = require("./routs/user.routs")
 const mongoose= require("mongoose")
-const app = express();
-
 
 mongoose.connect("mongodb+srv://urvashi:root@uvinfocluster.ko8y4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",()=>{
     console.log("connected")
 });
+const app = express();
+
+const bodyparser= require("body-parser")
+const userrout = require("./routs/user.routs")
+
+
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use("/user",userrout);
